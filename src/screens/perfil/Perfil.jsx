292232659fb5711ad6cbd706c86bf6e004cc8799
Eloi-Cortiella <eloicortiella@iesebre.com>
2026-01-22 18:58:14 +0100

    feature: Creació de la pantalla de perfil

---
+++ src/screens/perfil/Perfil.jsx
@@ -0,0 +1,15 @@
+import './Perfil.css'
+import profileLogo from '../../assets/perfilDefecte.png'
+
+function Perfil() {
+  return (
+    <>
+      <div>
+        <h1>Perfil</h1>
+        <img src={profileLogo} alt="Foto de perfil" />
+      </div>
+    </>
+  )
+}
+
+export default Perfil
